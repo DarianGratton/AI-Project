27fb8b2c8e5c5f4d95b6e1855f0b61316bb8b120
Darian Gratton <Dariangratton@gmail.com>
2018-02-12 15:40:14 -0800

    Hmmmm

---
+++ Java/src/Gui.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author Mike
+ *
+ */
+public class Gui {
+
+    
+    public void startGame(){
+        
+    }
+    
+    public void displayScore(Game g, boolean isBlack){
+        if(isBlack){
+            
+        }
+    }
+    
+    public void moveMarbles(Marble m1, int direction, boolean isBlack){
+        
+    }
+}
