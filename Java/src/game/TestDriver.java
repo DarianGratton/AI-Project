6251fa6aed890a8eea3ace306ca8b36e7316cdd2
Darian Gratton <Dariangratton@gmail.com>
2018-03-03 18:04:10 -0800

    Cleaned code, added comments, changed constructor for GameFrame so it takes a Game variable

--- Java/src/game/TestDriver.java
+++ Java/src/game/TestDriver.java
@@ -11,23 +11,21 @@ import javax.swing.JFrame;
 public class TestDriver {
 
     public static Game game = new Game();
-    public static final GameFrame frame = new GameFrame();
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-
+        final GameFrame frame;
+        final Game game1 = new Game();
+        
+        frame = new GameFrame(game1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 700);
         frame.setVisible(true);  
         
         Graphics g = frame.getGraphics();
         frame.paintComponents(g);
-        //Gui.drawBoard(game.getBoard(), frame, g);
-
-
-
 
         //Gui.drawBoard(test.getBoard(), frame, g);
 
