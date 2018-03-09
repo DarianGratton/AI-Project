24efee496a459b41e26bd0fa110173f4a3677090
aharaguchi <akharaguchi@gmail.com>
2018-03-09 15:18:46 -0800

    Added comments to IODriver.java

--- Java/src/game/IODriver.java
+++ Java/src/game/IODriver.java
@@ -7,14 +7,17 @@ import java.util.Scanner;
 
 /**
  * Created by Akemi on 3/6/2018.
+ *
+ * Run this in command line and specify one input file. The file's format must match the example given by the instructor upon release of Stage 2.
  */
 public class IODriver {
     public static void main(String[] args) {
         Scanner scan;
-        String readFile[] = {"", ""};
+        String readFile[] = {"", ""};                               //[0] = 'w' or 'b' ;;   [1] = 'A1b, B3b ....., F3w' etc
         int tracker = 0;
-        ArrayList<Marble> inputMarbles = new ArrayList<Marble>();
-        Board inputBoard = new Board();
+        ArrayList<Marble> inputMarbles = new ArrayList<Marble>();   //readFile[1] will eventually be put into this
+        ArrayList<Move> outputMoves = new ArrayList<Move>();        // return from sort function
+        Board inputBoard = new Board();                             //inputMarbles<Marble> will eventually go in here
 
 
         //Check that only one file was specified
@@ -24,27 +27,38 @@ public class IODriver {
         }
 
         try {
-            File file = new File(args[0]);
+            File file = new File(args[0]);                          //grab the only argument i.e. file name and put it in file
             scan = new Scanner(file);
-            while (scan.hasNextLine()) {
+            while (scan.hasNextLine()) {                            //file now read into readFile
                 readFile[tracker] = scan.nextLine();
                 tracker++;
             }
 
-            String[] splitInput = readFile[1].split(",");
+            String[] splitInput = readFile[1].split(",");   //Split fileInput[1] by the commas and put into another array
 
-            //Make marbles from input
-            tracker = 0;
+
+            //Make marble objects by taking in values from splitInput. Goes in ArrayList<Marble> inputMarbles
+            //turnAlphaToDigit = from 'A' - 'I', assign them numbers
+            //turnCharToInt = turn char '1' to integer 1.    Is there an easier way to do this?
+            //turn AlphaToBool = turn 'b' to   Marble.isBlack() = true ; w = false
+            int count = 0;
             for(String s: splitInput) {
-                inputMarbles.add(new Marble(turnAlphaToDigit(splitInput[tracker].charAt(0)),
-                        turnCharToInt(splitInput[tracker].charAt(1)), turnAlphaToBool(splitInput[tracker].charAt(2))));
-                tracker++;
+                inputMarbles.add(new Marble(turnAlphaToDigit(splitInput[count].charAt(0)),
+                        turnCharToInt(splitInput[count].charAt(1)),
+                        turnAlphaToBool(splitInput[count].charAt(2))));
+                count++;
             }
 
+            //For each Marble object in inputMarbles, put into inputBoard  Board object.
             for(int i = 0; i < inputMarbles.size(); ++i) {
                 inputBoard.add(inputMarbles.get(i));
                 //inputMarbles.get(i).toString();
             }
+
+            /**
+             * Code to get ArrayList<Moves> goes here:
+             * outputMoves = genPossibleMoves(inputBoard);
+             */
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -52,6 +66,8 @@ public class IODriver {
 
     }
 
+    // Turn n
+    // 'A' = 1; 'B' = 2; 'C' = 3; 'D' = 4; ........., 'I' = 9
     public static int turnAlphaToDigit(char alpha) {
         int n = 0;
         switch (alpha) {
@@ -131,5 +147,9 @@ public class IODriver {
                 break;
         }
         return n;
+    }
+
+    public static void writeOutput(ArrayList<Move> moves) {
+
     }
 }
